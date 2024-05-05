db0c97d3c89e3e3bf0b7eb8ad61f02dc8491e6f0
EgemenUzun <avniegemen1243@gmail.com>
2024-05-05 19:25:35 +0300

    imu calibiration to the north

--- lib/imu/imu.cpp
+++ lib/imu/imu.cpp
@@ -10,12 +10,12 @@
 #define INTERRUPT_PIN 2
 
 //----------- IMU CALIBRATION -----------
-#define ACCEL_OFFSET_X -1268
-#define ACCEL_OFFSET_Y -1393
-#define ACCEL_OFFSET_Z 1114
-#define GYRO_OFFSET_X 77
-#define GYRO_OFFSET_Y -48
-#define GYRO_OFFSET_Z 19
+#define ACCEL_OFFSET_X -1332
+#define ACCEL_OFFSET_Y -1363
+#define ACCEL_OFFSET_Z 1084
+#define GYRO_OFFSET_X 80
+#define GYRO_OFFSET_Y -51
+#define GYRO_OFFSET_Z 21
 
 //----------- TIMEOUTS -----------
 #define IMU_COMMUNICATION_TIMEOUT_IN_MILLISECONDS 100
